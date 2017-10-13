b9bc5e02da9157ff9a7301fa99c873a51b105f5f
Leessang <992275294@qq.com>
2017-10-13 15:33:21 +0800

    Initial Commit

---
+++ ARKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARKitDemo
+//
+//  Created by 刘凡 on 2017/10/13.
+//  Copyright © 2017年 KrystalName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ARKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARKitDemo
+//
+//  Created by 刘凡 on 2017/10/13.
+//  Copyright © 2017年 KrystalName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
